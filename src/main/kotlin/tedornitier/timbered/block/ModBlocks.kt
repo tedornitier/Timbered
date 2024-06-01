959e2788e5337bb0230fd6f01dcdb070c283211e
Alessandro Tedesco <alessandrotedd@gmail.com>
2024-06-01 16:37:22 +0200

    Handle adjacent blocks of the same type WIP

--- src/main/kotlin/tedornitier/timbered/block/ModBlocks.kt
+++ src/main/kotlin/tedornitier/timbered/block/ModBlocks.kt
@@ -1,22 +1,18 @@
 package tedornitier.timbered.block
 
 import net.minecraft.world.item.BlockItem
-import net.minecraft.world.item.CreativeModeTabs
 import net.minecraft.world.item.Item
 import tedornitier.timbered.Timbered
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.state.BlockBehaviour
 import net.neoforged.neoforge.registries.DeferredRegister
 
-// THIS LINE IS REQUIRED FOR USING PROPERTY DELEGATES
 import thedarkcolour.kotlinforforge.neoforge.forge.getValue
 
 object ModBlocks {
     val REGISTRY = DeferredRegister.createBlocks(Timbered.ID)
 
-    // If you get an "overload resolution ambiguity" error, include the arrow at the start of the closure.
+    // Register the custom block
     val SQUARE_TILE by REGISTRY.register("square_tile") { ->
-        Block(BlockBehaviour.Properties.of().lightLevel { 15 }.strength(3.0f))
+        SquareTileBlock()
     }
 
     // Register block items
