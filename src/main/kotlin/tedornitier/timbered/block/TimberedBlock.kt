cb5bb10597ce0075a86422ba88120e390f1fc3f0
Alessandro Tedesco <alessandrotedd@gmail.com>
2024-06-12 00:53:23 +0200

    Fix properties WIP

--- src/main/kotlin/tedornitier/timbered/block/TimberedBlock.kt
+++ src/main/kotlin/tedornitier/timbered/block/TimberedBlock.kt
@@ -3,4 +3,4 @@ package tedornitier.timbered.block
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.block.Blocks
 
-abstract class TimberedBlock : Block(Blocks.OAK_LOG.properties())
+abstract class TimberedBlock : Block(Properties.of().strength(3.0f))
