38c5a91236b6dfcd280437a20d748a436b9f4315
Alessandro Tedesco <alessandrotedd@gmail.com>
2024-06-12 00:50:05 +0200

    Add basic block abstractions

---
+++ src/main/kotlin/tedornitier/timbered/block/TimberedBlock.kt
@@ -0,0 +1,6 @@
+package tedornitier.timbered.block
+
+import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.Blocks
+
+abstract class TimberedBlock : Block(Blocks.OAK_LOG.properties())
