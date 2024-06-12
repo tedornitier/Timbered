b8f2de717c85e514bb2748565aa44f1f0c87ffa7
Alessandro Tedesco <alessandrotedd@gmail.com>
2024-06-12 18:05:44 +0200

    Fix diagonal block init

--- src/main/kotlin/tedornitier/timbered/block/SquareBlockDiagonal.kt
+++ src/main/kotlin/tedornitier/timbered/block/SquareBlockDiagonal.kt
@@ -1,32 +1,7 @@
 package tedornitier.timbered.block
 
-import net.minecraft.core.Direction
-import net.minecraft.world.item.context.BlockPlaceContext
-import net.minecraft.world.level.block.Block
-import net.minecraft.world.level.block.state.BlockState
-import net.minecraft.world.level.block.state.StateDefinition
-import net.minecraft.world.level.block.state.properties.BlockStateProperties
-import net.minecraft.world.level.block.state.properties.DirectionProperty
-
-class SquareBlockDiagonal : Block(Properties.of().strength(3.0f)) {
+class SquareBlockDiagonal : MirroredBlock() {
     companion object {
         const val NAME = "square_block_diagonal"
-        val FACING: DirectionProperty = BlockStateProperties.HORIZONTAL_FACING
-    }
-
-    init {
-        registerDefaultState(
-            stateDefinition.any().setValue(FACING, Direction.NORTH)
-        )
-    }
-
-    override fun createBlockStateDefinition(builder: StateDefinition.Builder<Block, BlockState>) {
-        builder.add(FACING)
-    }
-
-    override fun getStateForPlacement(context: BlockPlaceContext): BlockState {
-        val facing = context.horizontalDirection.opposite
-        return this.defaultBlockState()
-            .setValue(FACING, facing)
     }
 }
