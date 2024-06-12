cc03b97cc52c6ef6728b0f3d307147b42bd361c5
Alessandro Tedesco <alessandrotedd@gmail.com>
2024-06-12 21:49:04 +0200

    Add all wood types json

--- src/main/kotlin/tedornitier/timbered/block/TimberedWoodTypes.kt
+++ src/main/kotlin/tedornitier/timbered/block/TimberedWoodTypes.kt
@@ -1,5 +1,5 @@
 package tedornitier.timbered.block
 
 object TimberedWoodTypes { // TODO enum / sealed class
-    val woodTypes = listOf("oak") // TODO, "birch", "spruce", "jungle", "acacia", "dark_oak", "bamboo", "cherry", "crimson", "warped", "mangrove")
+    val woodTypes = listOf("oak", "birch", "spruce", "jungle", "acacia", "dark_oak", "bamboo", "cherry", "crimson", "warped", "mangrove")
 }
